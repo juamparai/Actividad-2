aabc97ccbc5e1ce9dcf036e1ce52acb73fbeeda1
unknown <juamparai@gmail.com>
2024-04-03 16:00:27 -0300

    Añadida segunda funcionalidad

--- my_modules.py
+++ my_modules.py
@@ -1,7 +1,20 @@
 def generate_structure (names, stat1, stat2, stat3):
-    """ This function zips four different lists in one making a list of uppered names from the first list, which has to be a string"""
+    """ This function zips four different lists into one making a list of uppered names from the first list, which has to be a string"""
     names_uppered = names.upper ()
     names_list = names_uppered.split(', ')
     zipped_file = zip (names_list, stat1, stat2, stat3)
-    return (zipped_file)
+    zipped_file_list =  list (zipped_file)
+    return (zipped_file_list)
 
+def calculate_top (team_data, stat_number):
+    """ This function recieves the calculates: 
+        The topscorer if entered 1
+        The player who avoided more goals if entered 2
+        The top assist if entered 3 """
+    max_stat = max(team_data, key=lambda x: x[stat_number])
+    return (max_stat)
+
+#def calculate_best (team_data):
+    """ This function returns the best player according to a weighted mean
+        Goal as 1.5, avoided goal as 1.25 and assist as 1 """
+    
--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import my_modules
 
-names = """ Agustin, Yanina, Andrés, Ariadna, Bautista, CAROLINA,
-CESAR, David, Diego, Dolores, DYLAN, ELIANA, Emanuel, Fabián, Noelia,
-Francsica', FEDERICO, Fernanda, GONZALO, Nancy """
+names = """ Agustin, Yanina, Andrés, Ariadna, Bautista, CAROLINA, CESAR, David, Diego, Dolores, DYLAN, ELIANA, Emanuel, Fabián, Noelia, Francsica, FEDERICO, Fernanda, GONZALO, Nancy """
 goals = [0, 10, 4, 0, 5, 14, 0, 0, 7, 2, 1, 1, 1, 5, 6, 1, 1, 2, 0, 11]
 goals_avoided = [0, 2, 0, 0, 5, 2, 0, 0, 1, 2, 0, 5, 5, 0, 1, 0, 2, 3, 0, 0]
 assists = [0, 5, 1, 0, 5, 2, 0, 0, 1, 2, 1, 5, 5, 0, 1, 0, 2, 3, 1, 0]
-team_data = my_modules.generate_structure (names, goals, goals_avoided, assists)
 
-#topscorer = my_modules.calculate_top(team_data, 1)
+team_data = my_modules.generate_structure (names, goals, goals_avoided, assists)
+topscorer = my_modules.calculate_top(team_data, 1)
+print (f'Goleador/a: ' ,topscorer)
+#most_influential = my_modules.calculate_best(team_data)
